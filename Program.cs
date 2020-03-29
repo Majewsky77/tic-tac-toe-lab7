726ea7a70d459bd670fa004dded9dd654536b42b
Maciej Majewski <maciej.majewski@microsoft.wsei.edu.pl>
2020-03-29 13:08:08 +0200

    Added players and game board

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tic_tac_toe_lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imieGraczaA = "";
+            string imieGraczaB = "";
+            char znakGraczaA = 'x';
+            char znakGraczaB = 'o';
+            char[,] plansza = new char[3, 3] {
+ { '1', '2', '3' },
+ { '4', '5', '6' },
+ { '7', '8', '9' }
+ };
+            Console.Write("Wpisz imie gracza A: ");
+            imieGraczaA = Console.ReadLine();
+            Console.Write("Wpisz imie gracza B: ");
+            imieGraczaB = Console.ReadLine();
+            // ...
+        }
+    }
+}
