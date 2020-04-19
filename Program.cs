8b6b16dca6f9f75f12a793332524168928f8f913
Maciej Majewski <maciej.majewski@microsoft.wsei.edu.pl>
2020-04-19 12:38:28 +0200

    Added function for drawing the board

--- Program.cs
+++ Program.cs
@@ -15,15 +15,42 @@ namespace tic_tac_toe_lab7
             char znakGraczaA = 'x';
             char znakGraczaB = 'o';
             char[,] plansza = new char[3, 3] {
- { '1', '2', '3' },
- { '4', '5', '6' },
- { '7', '8', '9' }
- };
+            { '1', '2', '3' },
+            { '4', '5', '6' },
+            { '7', '8', '9' }
+            };
+            char[,] planszaKopia = plansza.Clone() as char[,];
+
             Console.Write("Wpisz imie gracza A: ");
             imieGraczaA = Console.ReadLine();
             Console.Write("Wpisz imie gracza B: ");
             imieGraczaB = Console.ReadLine();
             // ...
+
+            // Petla na kolejne ruchy graczy
+            bool koniecGry = false;
+            for (int runda = 0; runda < plansza.Length; ++runda)
+            {
+                Console.Clear();
+                RysujPlansze(plansza);
+                Console.ReadKey();
+                // ...
+            }
+        }
+        ////////////////////////////////////////////////
+
+        static void RysujPlansze(char[,] plansza)
+        {
+            int wysokosc = plansza.GetLength(0);
+            int szerokosc = plansza.GetLength(1);
+
+            for (int i = 0; i < wysokosc; ++i)
+            {
+                for (int j = 0; j < szerokosc; ++j)
+                    Console.Write(plansza[i, j]);
+                Console.WriteLine();
+            }
+
         }
     }
 }
